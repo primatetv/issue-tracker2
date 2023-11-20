7f31d56e930966b62cf6126b78fa39f6ed4ce490
primatetv <tomas.vives@outlook.com>
2023-11-20 16:09:28 -0300

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-  title: z.string().min(3).max(255),
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255, "Description is required"),
   description: z.string().min(1),
 });
 
@@ -11,7 +14,7 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   });
